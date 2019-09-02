b0c2f94daa462c2eb31eab9afa9efe0b15fb6500
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2019-09-02 02:59:21 -0700

    Add files via upload

--- betterFaceid_iphonexsmax_12.4.sh
+++ betterFaceid_iphonexsmax_12.4.sh
@@ -1,5 +1,5 @@
-cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-812h.ca/ || exit 1
-mv /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-812h.ca/assets /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-812h.ca/assets_backup_01
+cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/ || exit 1
+mv /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/assets /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/assets_backup_01
 wget http://intocmd.com/betterFaceid/assets_iphonexsmax_12.4.zip
 unzip assets_iphonexsmax_12.4.zip
 rm assets_iphonexsmax_12.4.zip
