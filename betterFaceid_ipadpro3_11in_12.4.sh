712c97c614d3054b6e0b95ede36d079937c32e1f
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2019-09-02 01:25:25 -0700

    Add files via upload

--- betterFaceid_ipadpro3_11in_12.4.sh
+++ betterFaceid_ipadpro3_11in_12.4.sh
@@ -1,6 +1,6 @@
 cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/ || exit 1
 mv /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/assets /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/assets_backup_01
-wget http://intocmd.com/betterFaceid/assets.zip
-unzip assets.zip
-rm assets.zip
+wget http://intocmd.com/betterFaceid/assets_ipadpro3_11in_12.4.zip
+unzip assets_ipadpro3_11in_12.4.zip
+rm assets_ipadpro3_11in_12.4.zip
 killall -HUP SpringBoard
