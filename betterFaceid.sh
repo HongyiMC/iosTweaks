cfd6f865151102c4ba29af22009e1efc1f606ba5
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2019-09-02 00:03:46 -0700

    Add files via upload

---
+++ betterFaceid.sh
@@ -0,0 +1,5 @@
+mv /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/assets /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/assets_backup_01
+cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/
+wget http://intocmd.com/assets.zip
+unzip assets.zip
+rm assets.zip
