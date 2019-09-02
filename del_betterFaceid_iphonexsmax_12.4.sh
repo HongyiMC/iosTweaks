8d1c6ff1d859e6e3f223258c6ffe9cacd222448d
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2019-09-02 03:05:36 -0700

    Add files via upload

--- del_betterFaceid_iphonexsmax_12.4.sh
+++ del_betterFaceid_iphonexsmax_12.4.sh
@@ -1,5 +1,5 @@
 cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/ || exit 1
 rm -r assets
 mv /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/assets_backup_01 /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/assets
-rm /var/root/del_betterFaceid_ipadpro3_11in_12.4.sh
+rm /var/root/del_betterFaceid_iphonexsmax_12.4.sh
 killall -HUP SpringBoard
