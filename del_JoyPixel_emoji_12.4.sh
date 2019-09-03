124705d2dbd3ea105be166956b10c70ed150fb77
Hongyi Jiang <1428419198@qq.com>
2019-09-03 15:02:25 -0700

    Add JoyPixels install/uninstall bash

---
+++ del_JoyPixel_emoji_12.4.sh
@@ -0,0 +1,8 @@
+cd /System/Library/Fonts/Core/emoji_installed/ || exit 1
+rm -r /System/Library/Fonts/Core/emoji_installed/
+cd /
+cd /System/Library/Fonts/Core/
+rm -r AppleColorEmoji@2x.ttc
+mv /System/Library/Fonts/Core/AppleColorEmoji@2x_backup.ttc /System/Library/Fonts/Core/AppleColorEmoji@2x.ttc
+rm /var/root/del_JoyPixel_emoji_12.4.sh
+killall -HUP SpringBoard
