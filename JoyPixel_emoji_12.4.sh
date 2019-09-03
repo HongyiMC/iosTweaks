5409aff3a79397697a69b99c1f2b4895497b7235
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2019-09-03 14:50:59 -0700

    Delete JoyPixel_emoji_12.4.sh

--- JoyPixel_emoji_12.4.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-cd /System/Library/Fonts/Core/
-mkdir emoji_installed
-mv /System/Library/Fonts/Core/AppleColorEmoji@2x.ttc /System/Library/Fonts/Core/AppleColorEmoji@2x_backup.ttc
-wget http://intocmd.com/JoyPixels/AppleColorEmoji@2x.ttc
-rm /var/root/JoyPixel_emoji_12.4.sh
-killall -HUP SpringBoard
