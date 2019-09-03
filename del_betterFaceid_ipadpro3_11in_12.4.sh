aa4707c4355ec67f45b75e236d57c3caaa623f7b
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2019-09-02 23:12:37 -0700

    Delete del_betterFaceid_ipadpro3_11in_12.4.sh

--- del_betterFaceid_ipadpro3_11in_12.4.sh
+++
@@ -1,5 +0,0 @@
-cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/ || exit 1
-rm -r assets
-mv /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/assets_backup_01 /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/assets
-rm /var/root/del_betterFaceid_ipadpro3_11in_12.4.sh
-killall -HUP SpringBoard
