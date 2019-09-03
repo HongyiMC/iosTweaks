e28b88b180d539bdd9776c027025f008636236bd
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2019-09-02 23:36:04 -0700

    Add files via upload

--- del_betterFaceid_ipadpro3_11in_12.4.sh
+++ del_betterFaceid_ipadpro3_11in_12.4.sh
@@ -1,4 +1,4 @@
-set -e cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/
+set -ex cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/
 rm -r assets
 mv /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/assets_backup_01 /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@2x-j3xx.ca/assets
 rm /var/root/del_betterFaceid_ipadpro3_11in_12.4.sh
--- del_betterFaceid_iphonexsmax_12.4.sh
+++ del_betterFaceid_iphonexsmax_12.4.sh
@@ -1,4 +1,4 @@
-set -e cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/
+set -ex cd /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/
 rm -r assets
 mv /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/assets_backup_01 /System/Library/PrivateFrameworks/SpringBoardUIServices.framework/lock@3x-896h-d33.ca/assets
 rm /var/root/del_betterFaceid_iphonexsmax_12.4.sh
